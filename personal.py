557f827ddf3141edb06b75eaa35abdbb85a14bc0
Anumala Jithendra <84588609+anumala-jithendra@users.noreply.github.com>
2022-07-07 11:52:50 +0530

    Add files via upload

---
+++ personal.py
@@ -0,0 +1,30 @@
+import subprocess
+
+meta_data=subprocess.check_output(['netsh','wlan','show','profiles'])
+data=meta_data.decode('utf-8',errors="backslashreplace")
+data=data.split('\n')
+profiles=[]
+
+for i in data:
+    if"all user profile" in i:
+        i=i.split(":")
+        i=i[1]
+        i=i[1:-1]
+        profiles.append(i)
+        
+print("{:<30}| {:<}".format("wifi name","password"))
+print("-------------------------------------------")
+
+for i in profiles:
+    try:
+        results=subprocess.check_output(['netsh','wlan','show','profile',i,'key=clear'])
+        results=results.decode('utf-8',errors="backlashreplace")
+        results=results.split('\n')
+        results=[b.split(":")[1][1:-1] for b in results if "key content" in b]
+        try:
+            print("{:<30}| {:<}".format(i,results[0]))
+            
+        except IndexError:
+            print("{:<30}| {:<}".format(i,""))
+    except subprocess.CalledProcessError:
+        print("Encoding Error Occured")        
